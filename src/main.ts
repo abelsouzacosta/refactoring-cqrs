7296994478586fb122a6411af4a360273d320d54
Abel Junior <abel.costa@webjump.com.br>
2022-12-24 05:16:35 -0300

    refactor: separate query from modifier
    copy function to deal with queries

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
 // @ts-nocheck
+
+// todo: [refactor] - separate query from modifier
+// we should do this refactor to deal with functions that colateral effects
+// and to apply cqrs
 export function alertForMiscreant(people) {
+  for (const p of people) {
+    if ("Don" === p) {
+      setOffAlarms();
+      return "Don";
+    }
+    if ("John" === p) {
+      setOffAlarms();
+      return "John";
+    }
+  }
+
+  return "";
+}
+
+export function findMiscreant(people) {
   for (const p of people) {
     if ("Don" === p) {
       setOffAlarms();
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,9 +1,23 @@
-import { alertForMiscreant } from "../src/main";
+import { alertForMiscreant, findMiscreant } from "../src/main";
 
 describe("Alert for miscreant", () => {
   it("Should return a valid result", () => {
     let result = alertForMiscreant(["Don"]);
 
     expect(result).toStrictEqual("Don");
+  });
+});
+
+describe("Find Miscreant", () => {
+  it("Should return Don", () => {
+    let result = findMiscreant(["Don"]);
+
+    expect(result).toStrictEqual("Don");
+  });
+
+  it("Should return John", () => {
+    let result = findMiscreant(["John"]);
+
+    expect(result).toStrictEqual("John");
   });
 });
